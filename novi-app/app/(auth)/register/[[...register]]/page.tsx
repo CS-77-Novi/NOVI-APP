35224ee8acf6834b25e75fe17100f510dfc5d7c0
Chathila_Wijesinghe <chathilawijesinghe@gmail.com>
2025-12-21 22:26:01 +0530

    edited registration page

--- novi-app/app/(auth)/register/[[...register]]/page.tsx
+++ novi-app/app/(auth)/register/[[...register]]/page.tsx
@@ -19,7 +19,7 @@ const RegisterPage = () => {
                      
             </section>
 
-            <div className="mt-3">
+            <div className="mt-4">
                 <SignUp appearance={
                     {
                         baseTheme: neobrutalism
